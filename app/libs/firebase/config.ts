1be47c12de8959763db600223ece4c803b6ededa
iaavas <baralaavas@gmail.com>
2024-07-02 16:11:37 +0545

    Firebase authentication updated

--- app/libs/firebase/config.ts
+++ app/libs/firebase/config.ts
@@ -11,6 +11,6 @@ const firebaseConfig = {
 };
 
 const firebaseApp =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+  getApps().length == 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const firebaseAuth = getAuth(firebaseApp);
